f69112866041b628256e852ac26a3b9fff484476
liu.lu <liu.lu749@homefax.cn>
2019-11-15 18:52:23 +0800

    init

---
+++ src/main/java/com/code/codegenerator/GeneratorApplication.java
@@ -0,0 +1,25 @@
+package com.code.codegenerator;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class, args);
+    }
+}
